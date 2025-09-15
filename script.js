579f90a0a7a91bf6c766c7ff0dd0b669e08d4b5c
Affan-Rafique <affanrafique9@gmail.com>
2025-09-15 15:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ class Animal {
     }
 
     makeSound() {
-        return `The ${this.species} makes a sound`;
+        console.log(`The ${this.species} makes a sound`); // must log, not return
     }
 }
 
 class Dog extends Animal {
     bark() {
-        console.log("bark");
+        console.log("woof"); // test expects "woof", not "bark"
     }
 }
 
 class Cat extends Animal {
     purr() {
-        console.log("purr");
+        console.log("purr"); // test expects "purr"
     }
 }
-
 
 // Do not change the code below this line
 window.Animal = Animal;
